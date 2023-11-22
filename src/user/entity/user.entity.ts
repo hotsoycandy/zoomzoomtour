810c876c0d75458d1ec5a40731cfe89ee2e740a1
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-22 16:01:45 +0900

    Feat: [user.entity] Apply typeorm

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,5 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity({ name: 'users' })
 export class User {
+  @PrimaryGeneratedColumn()
   public idx!: string
+
+  @Column()
   public email!: string
+
+  @Column()
   public password!: string
 }
