45376358cef4519419c8cea1ae206624cece989b
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-22 15:23:52 +0900

    Feat: [user.service] Add signup

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { UserRepository } from './user.repository'
+import { User } from './entity/user.entity'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async signup(signupParams: {
+    email: string
+    password: string
+  }): Promise<User> {
+    const user = this.userRepository.newUser(signupParams)
+    return await this.userRepository.createUser(user)
+  }
+}
