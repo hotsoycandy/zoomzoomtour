d748b0e36700e3fb1bb21b357da43f21ac57234a
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-22 15:11:05 +0900

    Feat: [tour.repository] Create repository

---
+++ src/tour/tour.repository.ts
@@ -0,0 +1,8 @@
+import { Tour } from './entity/tour.entity'
+import { User } from 'src/user/entity/user.entity'
+
+export abstract class TourRepository {
+  abstract newTour(createTourParams: { title: string; seller: User }): Tour
+
+  abstract createTour(tour: Tour): Promise<Tour>
+}
