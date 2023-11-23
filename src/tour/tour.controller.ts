b803ffb55e647d39139d1d56615487207e6b7604
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-23 16:08:18 +0900

    Feat: [tour.controller] Add createBook

--- src/tour/tour.controller.ts
+++ src/tour/tour.controller.ts
@@ -1,14 +1,30 @@
 import { pick } from 'lodash'
-import { Controller, Post, Get, Body, Request, UseGuards } from '@nestjs/common'
-import { TourService } from './tour.service'
-import { JwtAuthGuard } from 'src/infrastructure/server/auth/jwt-auth.guard'
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Request,
+  UseGuards,
+  Param,
+} from '@nestjs/common'
 import { RequestWithUser } from 'src/infrastructure/server/auth/auth'
+import { JwtAuthGuard } from 'src/infrastructure/server/auth/jwt-auth.guard'
+// services
+import { TourService } from './tour.service'
+import { BookService } from 'src/book/book.service'
+// dtos
 import { TourDto } from './dto/tour.dto'
 import { CreateTourDto } from './dto/create-tour.dto'
+import { BookDto } from 'src/book/dto/book.dto'
+import { CreateBookDto } from 'src/book/dto/create-book.dto'
 
 @Controller('tours')
 export class TourController {
-  constructor(private readonly tourSerivce: TourService) {}
+  constructor(
+    private readonly tourSerivce: TourService,
+    private readonly bookService: BookService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('')
@@ -23,9 +39,23 @@ export class TourController {
     return TourDto.from(tour)
   }
 
-  @Get()
+  @Get('')
   async getTours(): Promise<TourDto[]> {
     const tours = await this.tourSerivce.getTours()
     return tours.map((tour) => TourDto.from(tour))
+  }
+
+  @Post('/:tourIdx/books')
+  async createBook(
+    @Request() req: RequestWithUser,
+    @Param('tourIdx') tourIdx: number,
+    @Body() createBookDto: CreateBookDto,
+  ): Promise<BookDto> {
+    const book = await this.bookService.createBook({
+      buyer: req.user,
+      tourIdx: tourIdx,
+      schedule: createBookDto.schedule,
+    })
+    return BookDto.from(book)
   }
 }
--- src/tour/tour.module.ts
+++ src/tour/tour.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { TourController } from './tour.controller'
 import { TourService } from './tour.service'
 import { TourRepositoryModule } from './persistence/tour.repository.module'
+import { BookModule } from 'src/book/book.module'
 
 @Module({
-  imports: [TourRepositoryModule],
+  imports: [TourRepositoryModule, forwardRef(() => BookModule)],
   controllers: [TourController],
   providers: [TourService],
+  exports: [TourService],
 })
 export class TourModule {}
