e547d0f17a4580247e15d5f6c0f032d3dae77e82
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-23 21:35:47 +0900

    Feat: [tour.module] Import DayoffModule

--- src/tour/tour.module.ts
+++ src/tour/tour.module.ts
@@ -3,9 +3,14 @@ import { TourController } from './tour.controller'
 import { TourService } from './tour.service'
 import { TourRepositoryModule } from './persistence/tour.repository.module'
 import { BookModule } from 'src/book/book.module'
+import { DayoffModule } from 'src/dayoff/dayoff.module'
 
 @Module({
-  imports: [TourRepositoryModule, forwardRef(() => BookModule)],
+  imports: [
+    TourRepositoryModule,
+    forwardRef(() => BookModule),
+    forwardRef(() => DayoffModule),
+  ],
   controllers: [TourController],
   providers: [TourService],
   exports: [TourService],
