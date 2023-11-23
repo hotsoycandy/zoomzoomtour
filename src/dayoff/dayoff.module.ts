34cf72d85ee1431b1f6f80d6fe6ec423f53e8f18
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-23 21:35:21 +0900

    Feat: [dayoff.module] Import TourModule

--- src/dayoff/dayoff.module.ts
+++ src/dayoff/dayoff.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { DayoffService } from './dayoff.service'
 import { DayoffRepositoryModule } from './persistence/dayoff.repository.module'
+import { TourModule } from 'src/tour/tour.module'
 
 @Module({
-  imports: [DayoffRepositoryModule],
+  imports: [DayoffRepositoryModule, forwardRef(() => TourModule)],
   providers: [DayoffService],
   exports: [DayoffService],
 })
