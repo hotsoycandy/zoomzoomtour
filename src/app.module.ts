21ec244fd1e677d148ee0d0058790a1e250763d0
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-23 12:59:09 +0900

    File: Create 'book' module, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TourModule } from './tour/tour.module'
 import { UserModule } from './user/user.module'
 import { DatabaseModule } from './infrastructure/database/database.module'
 import { AuthModule } from './infrastructure/server/auth/auth.module'
+import { BookModule } from './book/book.module'
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AuthModule } from './infrastructure/server/auth/auth.module'
     AuthModule,
     TourModule,
     UserModule,
+    BookModule,
   ],
 })
 export class AppModule {}
