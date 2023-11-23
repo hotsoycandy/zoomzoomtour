6cc1a0a6f10ddc2a0fd162363812b9fe953ca9d1
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-23 21:42:11 +0900

    Feat: [dayoff.service] Add createDayoff

--- src/dayoff/dayoff.service.ts
+++ src/dayoff/dayoff.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { pick } from 'lodash'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { TourService } from 'src/tour/tour.service'
+import { DayoffRepository } from './dayoff.repository'
+import { Dayoff, DayoffType } from './entity/dayoff.entity'
 
 @Injectable()
-export class DayoffService {}
+export class DayoffService {
+  constructor(
+    private readonly dayoffRepository: DayoffRepository,
+    private readonly tourService: TourService,
+  ) {}
+
+  async createDayoff(createDayoffParams: {
+    tourIdx: number
+    type: DayoffType
+    month?: number
+    date?: number
+    day?: number
+  }): Promise<Dayoff> {
+    const tour = await this.tourService.getTour(createDayoffParams.tourIdx)
+    if (tour === null) {
+      throw new NotFoundException('123')
+    }
+    const dayoff = this.dayoffRepository.newDayoff({
+      ...pick(createDayoffParams, ['type', 'month', 'date', 'day']),
+      tour,
+    })
+    return await this.dayoffRepository.createDayoff(dayoff)
+  }
+}
